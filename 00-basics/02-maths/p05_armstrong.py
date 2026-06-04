fc479824e4fad24acf5b8ac45968ad0724aa8eee
Shubham <shubham.workplaces@gmail.com>
2026-06-04 10:44:13 +0530

    armstrong
    Refactor is_armstrong method to use consistent variable names and remove duplicate code.

--- 00-basics/02-maths/p05_armstrong.py
+++ 00-basics/02-maths/p05_armstrong.py
@@ -1,7 +1,6 @@
 from p01_digit_counter import DigitCount
 
 class Armstrong:
-<<<<<<< HEAD
     def is_armstrong(self, num:int) -> bool:
         num_digits = DigitCount().digit_count(num)
         total = 0
@@ -12,20 +11,7 @@ class Armstrong:
             total += (last_digit) ** num_digits
             n = n // 10
         return num == total
-=======
-    def is_armstrong(self, n:int) -> bool:
-        num_digits = DigitCount().digit_count(n)
-        total = 0
-        temp = n
-
-        for _ in range(num_digits):
-            last_digit = temp % 10
-            total += (last_digit) ** num_digits
-            temp = temp // 10
-            
-        return n == total
->>>>>>> a4cfc17f344cbe023ed277e56174aba7fc9b2781
-
+        
 if __name__ == "__main__":
     obj = Armstrong()
     n = int(input("Enter a number: "))
