5086803380d019ee69a53dd2e6ff822a93805915
Shubham Choudhary <shubham.workplaces@gmail.com>
2026-06-04 10:38:41 +0530

    armstrong

--- 00-basics/02-maths/p05_armstrong.py
+++ 00-basics/02-maths/p05_armstrong.py
@@ -1,15 +1,15 @@
 from p01_digit_counter import DigitCount
 
 class Armstrong:
-    def is_armstrong(self, n:int) -> bool:
+    def is_armstrong(self, n: int) -> bool:
         num_digits = DigitCount().digit_count(n)
         total = 0
-        num = n
-
-        while num > 0:
-            last_digit = num % 10
-            total += (last_digit) ** num_digits
-            num = num // 10
+        temp = n
+        
+        for _ in range(num_digits):
+            last_digit = temp % 10
+            total += last_digit ** num_digits
+            temp = temp // 10
             
         return n == total
 
@@ -17,4 +17,4 @@ if __name__ == "__main__":
     obj = Armstrong()
     n = int(input("Enter a number: "))
     res = obj.is_armstrong(n)
-    print(f"Is {n} an armstrong number? {res}")
+    print(f"Is {n} an Armstrong number? {res}")
